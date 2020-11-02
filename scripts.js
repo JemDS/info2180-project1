89b684d0e04fa6e97f869ad22b860cae1743bd32
Deidre-Ann <d_jemison@hotmail.com>
2020-11-02 00:11:12 -0500

    Updated scripts.js file

--- scripts.js
+++ scripts.js
@@ -5,23 +5,19 @@ window.onload= function()
 var email = document.getElementById('email');
 var form = document.getElementsByTagName('form')[0];
 
-form.addEventListener("submit", (e) => { 
+form.addEventListener("submit", (e) => 
+	{ 
 	e.preventDefault();
 
-	console.log('valid email address!');
-
-   });
-
-	
-
-	if(email.value ===''){
+  	if(email.value ==''){
 		alert('Please enter valid email address!');
 	}
 
-	if(email.value == null){
+	else if(email.value == null){
 		alert('Please enter valid email address!');
 	}
 	else{
-		alert("Thank you! Your email address: " + email + " has been added to our mailing list!");
+		alert("Thank you! Your email address: " + email.value + " has been added to our mailing list!");
 	}
+	});
 }
