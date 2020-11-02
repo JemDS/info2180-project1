fe99bbbbb9c1c4b1dfd9704095406cecf8bacc0b
Deidre-Ann <d_jemison@hotmail.com>
2020-11-01 22:47:20 -0500

    Updated scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+window.onload= function()
+{
+
+const email = document.getElementByID('email');
+const form = document.getElementByTag('form');
+/*const errorElm = document.getElementByID('error');*/
+
+form.addEventListener('submit', (e) => { 
+
+	let message = [];
+	if(email.value ==='') || (email.value == null){
+		e.preventDefault();
+		alert('Please enter valid email address!');
+	}
+
+	/*if (message.length > 0) {
+		e.preventDefault();
+		errorElm.innerText = message.join(',')
+	}*/
+	else{
+		alert("Thank you! Your email address: " + email + " has been added to our mailing list!");
+	}
+
+}
+
+	/*document.getElementByClass('message').innerHTML = emails;*/
